17af0f6ee81bdc786da765d2def5835c7a88f00d
Brette <98058652+BrettefromNesUniverse@users.noreply.github.com>
2023-07-13 09:23:40 +0100

    Lightweight build is now functional

--- lightweight.py
+++ lightweight.py
@@ -1,6 +1,8 @@
 from zlib import crc32
 from sys import argv
-from urllib.request import urlretrieve
+import requests
+from os.path import exists
+from os import mkdir
 
 if __name__ == "__main__":
     if len(argv) <2 or argv[1]=='':
@@ -13,11 +15,34 @@ if __name__ == "__main__":
             rom = rom[16:]
             
         checksum = hex(crc32(rom))
-	try:
-		header = urlretrieve("https://github.com/BrettefromNesUniverse/HeaderTool/tree/main/headers", checksum)
-		with open((header[16:]).decode("utf-8"), "wb") as f:
-            		f.write(header[:16]+rom)
-	except: 
-		print("No header for this ROM.")
-            	exit()
-	        
+
+    header = requests.get(f"https://raw.githubusercontent.com/BrettefromNesUniverse/HeaderTool/main/headers/{checksum}.bin")
+    if header.status_code != 200:
+        print("No header for this ROM.")
+        exit()
+    
+    headers = [header,]
+    addend = 1
+    while not headers[-1] is None:
+        headers.append(requests.get(f"https://raw.githubusercontent.com/BrettefromNesUniverse/HeaderTool/main/headers/{checksum}_{addend}.bin"))
+        if headers[-1].status_code != 200: headers[-1] = None 
+        addend += 1
+    headers = headers[:-1]
+
+    if len(headers)-1:
+        print(*[f"\n{n+1}: {h.content[16:]}" for n, h in enumerate(iter(headers))])
+        choice = None 
+        while choice is None:
+            choice = input("Select which header to use: ")
+            try: choice = int(choice-1) 
+            except: choice = None
+
+            if choice < 0: choice = None
+            header = headers[choice]
+    header = header.content
+
+    if not exists("./output"): mkdir("output")
+        
+    with open(f"./output/{header[16:].decode('utf-8')}", "wb") as f:
+        f.write(header[:16] + rom)
+        
