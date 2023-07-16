c5872a3869ca879380d9e259d1861a98876b0b2f
Brette <98058652+BrettefromNesUniverse@users.noreply.github.com>
2023-07-17 00:02:01 +0100

    Preffered built-in for Python

---
+++ Python/lightweight.py
@@ -0,0 +1,30 @@
+from zlib import crc32
+from sys import argv, exit
+import urllib.request
+from os.path import exists
+from os import mkdir
+
+if __name__ == "__main__":
+    if len(argv) <2 or argv[1]=='':         # Arg validation
+        print("no file specified.")
+        exit()
+
+    with open(argv[1], "rb") as f:          # Arg2 access
+        rom = f.read()
+        if len(rom)&0x10: rom = rom[16:]    # remove header if present
+            
+        checksum = hex(crc32(rom))          # calculate headerless ROm
+
+    header = urllib.request.urlopen(f"https://raw.githubusercontent.com/BrettefromNesUniverse/HeaderTool/main/headers/{checksum}.bin")
+    if header.status != 200:           # retrieve file, and if succesful
+        print("No header for this ROM.")
+        exit()
+    
+    header = header.read()
+
+    if not exists("./output"): mkdir("output")                  # create output dir if not present
+        
+    with open(f"./output/{header[16:].decode('utf-8')}", "wb") as f:
+        f.write(header[:16] + rom)          # write renamed headered ROM with no-intro good name
+        
+else: raise Exception("No support")
