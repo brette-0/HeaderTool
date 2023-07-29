fc60bafa0e5a72c358d2126202b86e7a1616d821
Brette <98058652+BrettefromNesUniverse@users.noreply.github.com>
2023-07-29 22:09:30 +0100

    Slight opimizations

--- Windows/main.cpp
+++ Windows/main.cpp
@@ -7,37 +7,28 @@
         minimal library inclusion
 */
 
+#include <iostream>
 #include <string>                                               // filename
-
 #include <zlib.h>                                               // crc32 function
 #include <vector>                                               // big data types
-#include <stdint.h>                                             // data types
-
-#include <iostream>                                             // error reporting
-#include <cerrno>                                               // error handling
-
 #include <fstream>                                              // file handling
 #include <sys/stat.h>                                           // folder handling
 #include <curl/curl.h>                                          // access files
 
-#include <locale>
-#include <codecvt>
-
-
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::vector<uint8_t>* data) {
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::vector<char>* data) {
     /*
         Function to accept each byte from libcurl file retrieval
     */
     size_t totalSize = size * nmemb;                            // what is nmemb
-    uint8_t* byteContents = static_cast<uint8_t*>(contents);    // how does this work
+    char* byteContents = static_cast<char*>(contents);    // how does this work
     data->insert(data->begin(), byteContents, byteContents + totalSize);
     return totalSize;                                           // why return this
 }
-std::vector<uint8_t> getHDR(std::string const url){
+std::vector<char> getHDR(std::string const url){
     /*
         Method to retrieve file from repo
     */
-    std::vector<uint8_t> header;                                // create header object
+    std::vector<char> header;                                // create header object
     CURL* curl = curl_easy_init();                              // research
     if (!curl) {                                                // if curl failed to initialize
         // report fatal error within programme
@@ -51,13 +42,14 @@ std::vector<uint8_t> getHDR(std::string const url){
 
     CURLcode res = curl_easy_perform(curl);                     // research
     if (res != CURLE_OK) {
-        uint16_t httpCode = 0;                                  // initialize status code variable
+        long httpCode = 0;                                  // initialize status code variable
         curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
         if (httpCode == 404) {                                  // if file not found
             std::cerr << "Access Failure, likely bad Dump: HeaderTool only supports NES roms ending `.nes`" << std::endl;
         } else {                                                // otherwise unknown network error
             std::cerr << "Access Failure, Unknown reason: Check your network connection status or troubleshoot" << curl_easy_strerror(res) << std::endl;
         }
+        curl_easy_cleanup(curl);
         return {};}                                             // return empty vector
     curl_easy_cleanup(curl);                                    // Cleanup libcurl handle
     return header;                                              // return header contents
@@ -78,13 +70,13 @@ int main(const int argc, const char* argv[]){                   // accept sys ar
     }
 
     // read ROM file contents from PATH into ROM vector
-    std::vector<uint8_t> ROM = std::vector<uint8_t>(std::istreambuf_iterator<char>(ROMbuffer), std::istreambuf_iterator<char>());
+    std::vector<char> ROM = std::vector<char>(std::istreambuf_iterator<char>(ROMbuffer), std::istreambuf_iterator<char>());
     ROMbuffer.close();                                          // close ifstream
 
     // calculate file checksu as string, for filename.
-    std::string romChecksum = std::to_string(crc32(0, ROM.data(), ROM.size()));
+    std::string romChecksum = std::to_string(crc32(0, reinterpret_cast<const Bytef*>(ROM.data()), ROM.size()));
     if (mkdir("./output") != 0 && errno != EEXIST) {}           // ensure that ouput dir exists
-    std::vector<uint8_t> header = getHDR("https://raw.githubusercontent.com/BrettefromNesUniverse/HeaderTool/main/headers/" + romChecksum);
+    std::vector<char> header = getHDR("https://raw.githubusercontent.com/BrettefromNesUniverse/HeaderTool/main/headers/" + romChecksum);
     if (!header.size()){                                        // if we got emtpy results
         std::cin.ignore();                                      // wait for user input
         return 1;                                               // leave with exit code 1
@@ -95,7 +87,8 @@ int main(const int argc, const char* argv[]){                   // accept sys ar
 
     
     if (!outbuffer.is_open()){                                  // handle unknown error
-        std::cerr << "Failed to create File";
+        std::cerr << "Failed to create File";                   // report fatal error
+        std::cin.ignore();                                      // wait for user input
         return 1;
     }
     outbuffer << (headerstr.substr(0,16) + ROMstr);             // write contents
